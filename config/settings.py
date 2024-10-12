e475b0b2e6f3df57f10b7c91a864fde3bc2e7e7d
Artur <arturunkulov30@gmail.com>
2024-10-12 16:09:07 +0600

    updare coded

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+ALLOWED_HOSTS = ['185.117.149.144', 'localhost', '127.0.0.1', 'homelife-iwj3.onrender.com']
+
 
 
 
